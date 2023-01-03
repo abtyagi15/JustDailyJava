6dfe7fa1a0df06341bb6948bab6dc34db3a35dd2
Abhishek Tyagi <abtyagi150702@gmai.com>
2023-01-04 02:29:17 +0530

    Largest among two numbers

---
+++ LargestNum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class LargestNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the two numbers : ");
+        int num1 = sc.nextInt(), num2 = sc.nextInt();
+        int number = (num1>num2) ?  num1 : num2;
+        System.out.println("The greater number is : " + number);
+
+    }
+    
+}
